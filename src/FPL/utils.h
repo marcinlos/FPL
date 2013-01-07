f9d833526861d74f8b2b87265f83ed8d6e833e0c
Deltharis <michal.torba@gmail.com>
2013-01-07 12:54:18 +0100

    meh

--- src/FPL/utils.h
+++ src/FPL/utils.h
@@ -15,6 +15,21 @@ extern "C" {
 int FPL_highest_nonzero_bit(uint64_t x);
 
 
+#define FPL_NORMALIZE_MANTISSA_64(up) do {                      \
+        int pos = FPL_highest_nonzero_bit((up).m);              \
+        if (pos >= 0)                                           \
+        {                                                       \
+            int shift = pos - FPL_MANTISSA_SIZE_64;             \
+            if (shift > 0) (up).m >>= shift;                    \
+            else (up).m <<= (-shift);                           \
+            (up).e += shift;                                    \
+        }                                                       \
+        else (up).e = FPL_ZERO_EXP_64;                          \
+    } while (0)
+
+
+
+
 
 
 #ifdef __cplusplus
